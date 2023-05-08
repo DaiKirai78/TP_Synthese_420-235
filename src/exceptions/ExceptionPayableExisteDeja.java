55bbec13fff3f41125e9d1509165284f39b53050
Samuel <samleveille8@gmail.com>
2023-05-08 15:22:14 -0400

    Les classes NoeudPayable, ExceptionPayableExisteDeja et ExceptionPayableIntrouvable ont été complétés.

--- src/exceptions/ExceptionPayableExisteDeja.java
+++ src/exceptions/ExceptionPayableExisteDeja.java
@@ -1,5 +1,11 @@
 package exceptions;
 //
 // TODO 09-- Ajoutez le code nécessaire pour créer l'exception générée
-//         quand on essaie de créer un payabe avec un numéro de ID existant
+//         quand on essaie de créer un payable avec un numéro de ID existant
 //
+public class ExceptionPayableExisteDeja extends Exception {
+
+    public ExceptionPayableExisteDeja(int ID) {
+        super("L'ID " + ID + " existe déjà");
+    }
+}
--- src/exceptions/ExceptionPayableIntrouvable.java
+++ src/exceptions/ExceptionPayableIntrouvable.java
@@ -1,5 +1,11 @@
 package exceptions;
 //
 // TODO 08-- Ajoutez le code nécessaire pour créer l'exception générée
-//         quand on essaie de référer è un payable inexistant
+//         quand on essaie de référer à un payable inexistant
 //
+public class ExceptionPayableIntrouvable extends Exception {
+
+    public ExceptionPayableIntrouvable(int ID) {
+        super("L'ID " + ID + " n'existe pas");
+    }
+}
