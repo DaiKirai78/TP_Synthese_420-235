59e695548fe6a87b30bd8433d880d30aac19478e
Samuel <samleveille8@gmail.com>
2023-05-09 01:40:40 -0400

    La classe GestionnaireInventaire a été complétée.

--- src/inventaire/GestionnaireInventaire.java
+++ src/inventaire/GestionnaireInventaire.java
@@ -8,6 +8,28 @@ public class GestionnaireInventaire {
     // TODO 07-- Ajoutez tout le code nécessaire en vous basant sur le diagramme UML
     //         ainsi que la gestion des erreurs possibles si nécessaire
     //
+
+    private BaseDonnees baseDonnees;
+
+    public GestionnaireInventaire() {
+    }
+
+    public void ajouterPayable(Payable p) throws ExceptionPayableExisteDeja {
+        baseDonnees.inserer(p);
+    }
+
+    public void retirerPayable(int ID) throws ExceptionPayableIntrouvable {
+        baseDonnees.enlever(ID);
+    }
+
+    public void augmenterEcheancePayable(int ID, int e) {
+        baseDonnees.trouverParID(ID).augmenterEcheance(e);
+    }
+
+    public void diminuerEcheancePayable(int ID, int e) throws ExceptionEcheanceInsuffisante {
+        baseDonnees.trouverParID(ID).diminuerEcheance(e);
+    }
+
     public Payable getPayable(int ID) throws ExceptionPayableIntrouvable {
         Payable payable = baseDonnees.trouverParID(ID);
         if (payable != null)
